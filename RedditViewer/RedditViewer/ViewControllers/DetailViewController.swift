6e485546812c81c927080a192f6aa151e10fd9ed
Lucia Ginart <lucia.ginart@gmail.com>
2019-08-31 10:19:36 -0300

    New splitVC proyect

---
+++ RedditViewer/RedditViewer/ViewControllers/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  RedditViewer
+//
+//  Created by Lucia Belen Ginart on 8/31/19.
+//  Copyright © 2019 Lucia Belen Ginart. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ RedditViewer/RedditViewer/ViewControllers/MasterViewController.swift
@@ -0,0 +1,58 @@
+//
+//  MasterViewController.swift
+//  RedditViewer
+//
+//  Created by Lucia Belen Ginart on 8/31/19.
+//  Copyright © 2019 Lucia Belen Ginart. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+
+    var detailViewController: DetailViewController? = nil
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let split = splitViewController {
+            let controllers = split.viewControllers
+            detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+        }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let splitViewController = splitViewController {
+            clearsSelectionOnViewWillAppear = splitViewController.isCollapsed
+        }
+    }
+
+    // MARK: - Segues
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+}
+
+// MARK: - Table View
+extension MasterViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        return cell
+    }
+}
+
